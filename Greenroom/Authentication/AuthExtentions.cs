d6f8ce0964c4b5a6c1b058e56269af2e4f3c4a3c
Matthew Kirkendall <kirk.viola@gmail.com>
2023-06-26 15:57:36 -0500

    Add project files.

---
+++ Greenroom/Authentication/AuthExtentions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Greenroom.Authentication
+{
+    public static class AuthExtentions
+    {
+
+    }
+}
