b92f8d38dfe46ae22bda8ba94e6b6ae81e4ec2f6
Matthew Kirkendall <kirk.viola@gmail.com>
2023-06-27 19:03:04 -0500

    Initial Projects laid out

---
+++ Greenroom.Infrastructure/GreenroomDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Greenroom.Core.Interfaces;
+using Greenroom.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Greenroom.Infrastructure;
+
+public class GreenroomDbContext : DbContext, IGreenroomDbContext
+{
+    public DbSet<User> Users { get; set; }
+}
